384656b0e8d79009decc3ae39cf7d5a6d7114a36
Zoey Dao <yendao.zoey@gmail.com>
2018-10-13 23:17:12 +0700

    fix yt download max

--- Lab2/homework/itunes_songs.py
+++ Lab2/homework/itunes_songs.py
@@ -37,7 +37,6 @@ from youtube_dl import YoutubeDL
 
 options = {
     'default_search': 'ytsearch', # tell downloader to search instead of directly downloading
-    'max_downloads': 1, # Tell downloader to download only the first entry (audio)
     'format': 'bestaudio/audio'
 }
 dl = YoutubeDL(options)
