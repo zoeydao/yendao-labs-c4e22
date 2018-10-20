6bb0050e015c7976ac7fd62545ebdc5a9e9410c7
Zoey Dao <yendao.zoey@gmail.com>
2018-10-20 22:46:07 +0700

    lab3 homework

--- Lab3/homework/serious12.py
+++ Lab3/homework/serious12.py
@@ -2,8 +2,10 @@ from serious11 import is_inside
 
 l1 = [100, 120]
 l2 = [140, 60, 100, 200]
+l3 = [200, 120]
+l4 = [140, 60, 100, 200]
 
-if is_inside(l1,l2) == False:
+if is_inside(l1,l2) == False and is_inside(l3,l4) == True:
     print('Your function is correct')
 else:
     print('Your function is wrong')
